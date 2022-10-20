61de457bbf0b11fd46bd076711615de81f98bc23
taiello19 <thomasaiello19@gmail.com>
2022-10-20 18:31:24 -0400

    added test code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("test");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+
+});
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`);
+});
