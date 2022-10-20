7692896d529e27796e9a88c7db4f7e9979a8bfb9
taiello19 <thomasaiello19@gmail.com>
2022-10-20 19:42:16 -0400

    added html and css files

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use('/', express.static('static'));
+
+app.get('/api', (req, res) => {
     res.send('Hello World')
 
 });
