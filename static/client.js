04040a47f7436349a9294bf4e0583fd7026a2dc7
taiello19 <thomasaiello19@gmail.com>
2022-11-02 12:57:41 -0400

    added getting data from the server based on search input

--- static/client.js
+++ static/client.js
@@ -8,37 +8,37 @@ trackSearch.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const track = document.getElementById("trackInput").value;
+    console.log(track);
 
     fetch("http://localhost:3000/trackName?trackInputName=" + track, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
-        .then(res => {
-            res.text()
-        })
-        .then(data => {
-            console.log(data)
-            document.getElementById("display");
-        })
-        .catch(err => console.log(err));
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 });
 
 artistSearch.addEventListener('submit', function (e) {
     //prevent refresh page
     e.preventDefault();
 
-    const track = document.getElementById("trackInput").value;
+    const artist = document.getElementById("artistInput").value;
 
-    fetch("http://localhost:3000/artist?artistInputName=" + track, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
-        .then(res => {
-
-            res.text()
-        })
-        .then(data => {
-
-            console.log(data)
-            document.getElementById("display");
-        })
-        .catch(err => console.log(err));
+    fetch("http://localhost:3000/artist?artistInputName=" + artist, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 });
 
+albumSearch.addEventListener('submit', function (e) {
+    //prevent refresh page
+    e.preventDefault();
+
+    const album = document.getElementById("albumInput").value;
+
+    fetch("http://localhost:3000/album?albumInputName=" + album, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+});
 
 
 
