3cd614f2324a531512d80a1f741bbcc295d6f805
taiello19 <thomasaiello19@gmail.com>
2022-11-02 16:48:15 -0400

    added search by track frontend functionality + html and css

--- static/client.js
+++ static/client.js
@@ -12,7 +12,97 @@ trackSearch.addEventListener('submit', function (e) {
 
     fetch("http://localhost:3000/trackName?trackInputName=" + track, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(function (data) {
+            let div = document.getElementById("divTbl");
+
+            if (div.hasChildNodes()) {
+                document.getElementById('table').remove();
+            }
+            const table = document.createElement("table");
+            table.id = "table";
+            table.className = "table";
+
+            div.appendChild(table);
+
+            let namesRow = document.createElement("tr");
+            table.appendChild(namesRow);
+
+            let header1 = document.createElement("th");
+            header1.appendChild(document.createTextNode("albumID"));
+
+            let header2 = document.createElement("th");
+            header2.appendChild(document.createTextNode("albumTitle"));
+
+            let header3 = document.createElement("th");
+            header3.appendChild(document.createTextNode("artistID"));
+
+            let header4 = document.createElement("th");
+            header4.appendChild(document.createTextNode("artistName"));
+
+            let header5 = document.createElement("th");
+            header5.appendChild(document.createTextNode("trackDur"));
+
+            let header6 = document.createElement("th");
+            header6.appendChild(document.createTextNode("trackID"));
+
+            let header7 = document.createElement("th");
+            header7.appendChild(document.createTextNode("track#"));
+
+            let header8 = document.createElement("th");
+            header8.appendChild(document.createTextNode("trackTitle"));
+
+            namesRow.appendChild(header1);
+            namesRow.appendChild(header2);
+            namesRow.appendChild(header3);
+            namesRow.appendChild(header4);
+            namesRow.appendChild(header5);
+            namesRow.appendChild(header6);
+            namesRow.appendChild(header7);
+            namesRow.appendChild(header8);
+
+            for (j = 0; j < data.length; j++) {
+                const dataRow = document.createElement('tr');
+                table.appendChild(dataRow);
+
+
+                let tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].albumID));
+                dataRow.appendChild(tData);
+
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].albumTitle));
+                dataRow.appendChild(tData);
+
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].artistID));
+                dataRow.appendChild(tData);
+
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].artistName));
+                dataRow.appendChild(tData);
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].trackDuration));
+                dataRow.appendChild(tData);
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].trackID));
+                dataRow.appendChild(tData);
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].trackNumber));
+                dataRow.appendChild(tData);
+
+                tData = document.createElement('td');
+                tData.appendChild(document.createTextNode(data[j].trackTitle));
+                dataRow.appendChild(tData);
+
+            }
+
+        })
         .catch(err => console.log(err))
 });
 
@@ -23,9 +113,9 @@ artistSearch.addEventListener('submit', function (e) {
     const artist = document.getElementById("artistInput").value;
 
     fetch("http://localhost:3000/artist?artistInputName=" + artist, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 });
 
 albumSearch.addEventListener('submit', function (e) {
@@ -35,9 +125,9 @@ albumSearch.addEventListener('submit', function (e) {
     const album = document.getElementById("albumInput").value;
 
     fetch("http://localhost:3000/album?albumInputName=" + album, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 });
 
 
