8d1fb65e0a5718273113c116ad4e421d9993623b
taiello19 <thomasaiello19@gmail.com>
2022-11-02 12:34:35 -0400

    added requests for tasks 3,4,5

--- static/client.js
+++ static/client.js
@@ -3,14 +3,43 @@ const trackSearch = document.getElementById("trackSearch");
 const albumSearch = document.getElementById("albumSearch");
 
 
-trackSearch.addEventListener('submit', function(e) {
+trackSearch.addEventListener('submit', function (e) {
     //prevent refresh page
     e.preventDefault();
 
     const track = document.getElementById("trackInput").value;
-    
-    fetch("http://localhost:3000/trackName?trackInputName=" + track,  {method: 'GET', headers: new Headers({'Content-Type': 'application/json'})})
-        .then(res => res.text())
-        .then(data => console.log(data))
+
+    fetch("http://localhost:3000/trackName?trackInputName=" + track, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+        .then(res => {
+            res.text()
+        })
+        .then(data => {
+            console.log(data)
+            document.getElementById("display");
+        })
         .catch(err => console.log(err));
 });
+
+artistSearch.addEventListener('submit', function (e) {
+    //prevent refresh page
+    e.preventDefault();
+
+    const track = document.getElementById("trackInput").value;
+
+    fetch("http://localhost:3000/artist?artistInputName=" + track, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+        .then(res => {
+
+            res.text()
+        })
+        .then(data => {
+
+            console.log(data)
+            document.getElementById("display");
+        })
+        .catch(err => console.log(err));
+});
+
+
+
+
+
