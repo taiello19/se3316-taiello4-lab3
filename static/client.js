5ac663c1dd96f3390727a72174a792464cf0ba5b
taiello19 <thomasaiello19@gmail.com>
2022-11-04 11:57:19 -0400

    added displaying playlist functionality

--- static/client.js
+++ static/client.js
@@ -3,9 +3,130 @@ const trackSearch = document.getElementById("trackSearch");
 const albumSearch = document.getElementById("albumSearch");
 const playlistID = document.getElementById("playlistID");
 const listText = document.getElementById("listName");
+const comboBox = document.getElementById("playlistComboBox");
+
+
 
 window.onload = populateRefresh();
 
+comboBox.addEventListener('submit', function (e) {
+    //prevent refresh page
+    e.preventDefault();
+    const selection = document.getElementById("playlistBox").value;
+
+    
+    fetch("http://" + window.location.host + "/loadPlaylist?playlist=" + selection, { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+    .then(res => res.json())
+    .then(function (data) {
+
+        let div = document.getElementById("divTbl");
+
+        if (div.hasChildNodes()) {
+            document.getElementById('table').remove();
+            document.getElementById('h1').remove();
+        }
+        
+        const table = document.createElement("table");
+        table.id = "table";
+        table.className = "table";
+
+        const topText = document.createElement("h1");
+        topText.id = "h1";
+        topText.appendChild(document.createTextNode(`Displaying playlist: ${selection}`));
+        div.appendChild(topText);
+
+        div.appendChild(table);
+
+        let namesRow = document.createElement("tr");
+        table.appendChild(namesRow);
+
+        let header1 = document.createElement("th");
+        header1.appendChild(document.createTextNode("albumID"));
+
+        let header2 = document.createElement("th");
+        header2.appendChild(document.createTextNode("albumTitle"));
+
+        let header3 = document.createElement("th");
+        header3.appendChild(document.createTextNode("artistID"));
+
+        let header4 = document.createElement("th");
+        header4.appendChild(document.createTextNode("artistName"));
+
+        let header5 = document.createElement("th");
+        header5.appendChild(document.createTextNode("trackDuration"));
+
+        let header6 = document.createElement("th");
+        header6.appendChild(document.createTextNode("trackID"));
+
+        let header7 = document.createElement("th");
+        header7.appendChild(document.createTextNode("trackNum"));
+
+        let header8 = document.createElement("th");
+        header8.appendChild(document.createTextNode("trackTitle"));
+
+        let header9 = document.createElement("th");
+        header9.appendChild(document.createTextNode("trackGenres"));
+
+        namesRow.appendChild(header1);
+        namesRow.appendChild(header2);
+        namesRow.appendChild(header3);
+        namesRow.appendChild(header4);
+        namesRow.appendChild(header5);
+        namesRow.appendChild(header6);
+        namesRow.appendChild(header7);
+        namesRow.appendChild(header8);
+        namesRow.appendChild(header9);
+
+        for (j = 0; j < data.length; j++) {
+            const dataRow = document.createElement('tr');
+            table.appendChild(dataRow);
+
+
+            let tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].albumID));
+            dataRow.appendChild(tData);
+
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].albumTitle));
+            dataRow.appendChild(tData);
+
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].artistID));
+            dataRow.appendChild(tData);
+
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].artistName));
+            dataRow.appendChild(tData);
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].trackDuration));
+            dataRow.appendChild(tData);
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].trackID));
+            dataRow.appendChild(tData);
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].trackNum));
+            dataRow.appendChild(tData);
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].trackTitle));
+            dataRow.appendChild(tData);
+
+            tData = document.createElement('td');
+            tData.appendChild(document.createTextNode(data[j].trackGenres));
+            console.log(tData)
+            dataRow.appendChild(tData);
+
+        }
+
+    })
+    .catch(err => console.log(err))
+});
 
 
 
@@ -48,6 +169,8 @@ function populateRefresh() {
 
 
 
+
+
 playlistID.addEventListener('submit', function (e) {
     //prevent refresh page
     e.preventDefault();
@@ -55,21 +178,21 @@ playlistID.addEventListener('submit', function (e) {
     const trackID = document.getElementById("trackPlaylist").value;
     let ID = trackID.toString().split(",");
 
-    for(i=0; i < ID.length; i++){
-    fetch("http://" + window.location.host + "/track/" + ID[i], { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
-        .then(res => res.json())
-        .then(function (data) {
-            let newArray = [];
-            newArray.push(data);
-            console.log(data);
-        })
-        .catch(err => console.log(err))
+    for (i = 0; i < ID.length; i++) {
+
+        fetch("http://" + window.location.host + "/track/" + ID[i], { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' }) })
+            .then(res => res.json())
+            .then(data => {
+                fetch("http://" + window.location.host + "/playlist/" + playlistBox, { method: 'PUT', body: JSON.stringify(data), headers: new Headers({ 'Content-Type': 'application/json' }) })
+                    .then(res => res.json())
+                    .then(data => {
+                    })
+                    .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err));
     }
-
-    
-
-
 });
+
 
 
 
